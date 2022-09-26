903eb8a2ebb30f0cb330d2ce2da3269c9d3b0f99
alpha@DESKTOP-6F6AEHR <blkcdev@gmail.com>
2022-09-26 15:02:24 -0500

    created code for rajini coin

---
+++ contracts/RajiniCoin.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+  pragma solidity ^0.8.0;
+
+  import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+  import "@openzeppelin/contracts/access/Ownable.sol";
+
+  abstract contract RajiniCoin is ERC20, Ownable {
+
+}
