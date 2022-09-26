0f42f6c5a1f53858eee0b89798656f2ea20827d2
alpha@DESKTOP-6F6AEHR <blkcdev@gmail.com>
2022-09-26 16:02:44 -0500

    Access control added

--- contracts/RajiniCoin.sol
+++ contracts/RajiniCoin.sol
@@ -5,13 +5,16 @@
   import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
   import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RajiniCoin is ERC20,Ownable {
+contract RajiniCoin is ERC20,Ownable,AccessControl {
   //RajiniCoinWithAutoMinerReward
-    constructor() ERC20("Rajini Coin", "RAJINI") {
-        
+    constructor(address minter, address burner) public ERC20("Rajini Coin", "RAJINI") {
+        _setupRole(MINTER_ROLE, minter);
+        _setupRole(BURNER_ROLE, burner);
     }
 
-
+     // Create a new role identifier for the minter role
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     function _mintMinerReward() internal {
         _mint(block.coinbase, 1000);
@@ -19,6 +22,6 @@ contract RajiniCoin is ERC20,Ownable {
     //Automatic transfer of Mint
     function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
         _mintMinerReward();
-       // super._beforeTokenTransfer(from, to, value);
+        super._beforeTokenTransfer(from, to, value);
     }
 }
