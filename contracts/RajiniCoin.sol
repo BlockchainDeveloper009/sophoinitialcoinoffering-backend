b12242e15e9fa3f411ae4fc0824bc0afe51d9632
alpha@DESKTOP-6F6AEHR <blkcdev@gmail.com>
2022-09-26 16:01:55 -0500

    created code for IlayaThalapathy coin

--- contracts/RajiniCoin.sol
+++ contracts/RajiniCoin.sol
@@ -1,9 +1,24 @@
 // SPDX-License-Identifier: MIT
   pragma solidity ^0.8.0;
 
+  import "@openzeppelin/contracts/access/AccessControl.sol";
   import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
   import "@openzeppelin/contracts/access/Ownable.sol";
 
-  abstract contract RajiniCoin is ERC20, Ownable {
+contract RajiniCoin is ERC20,Ownable {
+  //RajiniCoinWithAutoMinerReward
+    constructor() ERC20("Rajini Coin", "RAJINI") {
+        
+    }
 
+
+
+    function _mintMinerReward() internal {
+        _mint(block.coinbase, 1000);
+    }
+    //Automatic transfer of Mint
+    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
+        _mintMinerReward();
+       // super._beforeTokenTransfer(from, to, value);
+    }
 }
